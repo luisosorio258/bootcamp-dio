4738b349400181ad4f068a5fd83858331e701fe5
Luis Osorio <64318203+luisosorio258@users.noreply.github.com>
2021-03-15 19:34:38 -0300

    new

---
+++ curso-html5/introducao-js/main.js
@@ -0,0 +1,42 @@
+function click(){
+    alert("Obrigado por clicar")
+}
+
+
+
+/*let nome = 'Luis Osorio'
+let idade = 25
+let idade2 = 10
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)*/
+
+/*var lista = ["maça", "pera", "laranja"]
+lista.push("uva")
+lista.pop("pera")
+console.log(lista)*/
+
+/*let fruta = {nome:"maça", cor="vermelha"}
+console.log(fruta)*/
+
+//console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+
+/*function soma(n1,n2){
+     return n1+n2
+}
+
+alert(soma(5,10))*/
+
+/*function validaIdade (idade){
+    let validar
+    if(idade >= 18){
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar
+}
+
+console.log(20)*/
+
+
+
